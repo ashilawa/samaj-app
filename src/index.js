3541235f8624629cbc163885b461f8a368d9dab0
Akshay Shilawane <akshayshilawane@Akshays-MacBook-Pro.local>
2020-01-02 08:56:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+//import "./index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Featured
+import Index from "./views/Index";
+import About from "./views/About";
+import Projects from "./views/Projects";
+import Resume from "./views/Resume";
+import Stats from "./views/Stats";
+import Contact from "./views/Contact";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import NotFound from "./views/NotFound";
+
+// All of our CSS
+import "./static/css/main.scss";
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/stats" component={Stats} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/resume" component={Resume} />
+      {/* Only useful in development mode */}
+      <Route component={NotFound} status={404} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
