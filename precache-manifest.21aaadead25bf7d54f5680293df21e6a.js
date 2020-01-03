d74cb9c8a4b9244c17816ed8ea46e46ec2eeef37
Shilawane1 <503134803@logon.ds.ge.com>
2020-01-03 15:04:56 +0530

    Updates

---
+++ precache-manifest.21aaadead25bf7d54f5680293df21e6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e15af4a8b86b7eb465c032276f4451e",
+    "url": "/ashilawa/samaj-app/index.html"
+  },
+  {
+    "revision": "6572d4fb5de7f8aa3b42",
+    "url": "/ashilawa/samaj-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "90309bd326a0061f107a",
+    "url": "/ashilawa/samaj-app/static/css/main.f78f541c.chunk.css"
+  },
+  {
+    "revision": "6572d4fb5de7f8aa3b42",
+    "url": "/ashilawa/samaj-app/static/js/2.468a8630.chunk.js"
+  },
+  {
+    "revision": "061804563a883c557024150a76f7a969",
+    "url": "/ashilawa/samaj-app/static/js/2.468a8630.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90309bd326a0061f107a",
+    "url": "/ashilawa/samaj-app/static/js/main.01b2fc28.chunk.js"
+  },
+  {
+    "revision": "564ce881fa4deee3328c",
+    "url": "/ashilawa/samaj-app/static/js/runtime-main.80106ada.js"
+  },
+  {
+    "revision": "1d759c4a5771156fa186643ec180c217",
+    "url": "/ashilawa/samaj-app/static/media/about.1d759c4a.md"
+  }
+]);
