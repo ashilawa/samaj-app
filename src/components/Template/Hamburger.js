51c7364c948c2b3ade34b2791ad6d42221023cd2
Akshay Shilawane <akshayshilawane@Akshays-MacBook-Pro.local>
2020-02-03 23:41:02 +0530

    responsive fix

--- src/components/Template/Hamburger.js
+++ src/components/Template/Hamburger.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import Menu from "react-burger-menu/lib/menus/slide";
+import Menu from 'react-burger-menu/lib/menus/slide';
 
-import routes from "../../data/routes";
+import routes from '../../data/routes';
 
 const Hamburger = () => {
   const [open, setOpen] = useState(false);
@@ -14,25 +14,21 @@ const Hamburger = () => {
         <ul>
           {open ? (
             <li className="menu close-menu">
-              <div onClick={() => setOpen(!open)} className="menu-hover">
-                &#10005;
-              </div>
+              <div onClick={() => setOpen(!open)} className="menu-hover">&#10005; Close</div> 
             </li>
           ) : (
             <li className="menu open-menu">
-              <div onClick={() => setOpen(!open)} className="menu-hover">
-                &#9776;
-              </div>
+              <div onClick={() => setOpen(!open)} className="menu-hover">&#9776; Menu</div> 
             </li>
           )}
         </ul>
       </nav>
-      <Menu right isOpen={open} width={"50%"}>
+      <Menu right isOpen={open} width={"70%"}>
         <ul className="hamburger-ul">
-          {routes.map(l => (
+          {routes.map((l) => (
             <li key={l.label}>
               <Link to={l.path} onClick={() => setOpen(!open)}>
-                <h3 className={l.index && "index-li"}>{l.label}</h3>
+                <h3 className={l.index && 'index-li'}>{l.label}</h3>
               </Link>
             </li>
           ))}
